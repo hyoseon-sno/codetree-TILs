803db28a9d8cb6130478f8537428f9c05ec6bc65
kimhyoseon <kimhyoseon928@gmail.com>
2024-07-12 12:00:26 +0900

    Title: 5의 배수의 합, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240712/5의 배수의 합/sum-of-specific-condition-multiple.py
+++ 240712/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -1,8 +1,11 @@
-a,b=map(int,input().split())
-count=0
+a, b = map(int, input().split())
+count = 0
 
-for i in range(a,b+1):
-    if i%5==0:
-        count+=i
+start = min(a, b)
+end = max(a, b)
+
+for i in range(start, end + 1):
+    if i % 5 == 0:
+        count += i
 
 print(count)
