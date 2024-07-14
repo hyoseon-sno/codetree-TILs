b6ec4a6f6ed0510292767d3d3a557adc905298fc
kimhyoseon <kimhyoseon928@gmail.com>
2024-07-14 10:31:15 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240714/20대의 평균나이/average-age-of-20.py
+++ 240714/20대의 평균나이/average-age-of-20.py
@@ -1,16 +1,14 @@
-a,b=0,0
+a=0
+b=0
 count=0
 
 while True:
     n=int(input())
-    
+
     if n >29:
         break;
-
     if n<30:
         count+=1
         a+=n
-
 b=a/count
-
 print(f"{b:.2f}")
