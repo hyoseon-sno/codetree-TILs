21e09c839946b00bb09440b1b24fc322c7bf4d6e
kimhyoseon <kimhyoseon928@gmail.com>
2024-07-14 10:36:30 +0900

    Title: 20대의 평균나이, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240714/20대의 평균나이/average-age-of-20.py
+++ 240714/20대의 평균나이/average-age-of-20.py
@@ -1,19 +1,16 @@
-a = 0
-count = 0
+a=0
+b=0
+count=0
 
-try:
-    while True:
-        n = int(input())
-        if n > 29:
-            break
-        if n < 30:
-            count += 1
-            a += n
-except EOFError:
-    pass
+while True:
+    n=int(input())
 
-if count > 0:
-    b = a / count
-    print(f"{b:.2f}")
-else:
-    print("No valid input received")
+    if n >29:
+        break;
+    if n <10:
+        break;
+    if n<30:
+        count+=1
+        a+=n
+b=a/count
+print(f"{b:.2f}")
