cfc3c97552fc2efa190482893dedf184d4d7d2b2
kimhyoseon <kimhyoseon928@gmail.com>
2024-07-31 18:48:20 +0900

    Title: 사칙연산 함수, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/사칙연산 함수/quadratic-operations-function.py
+++ 240731/사칙연산 함수/quadratic-operations-function.py
@@ -2,37 +2,29 @@ a, o, c = input().split()
 a = int(a)
 c = int(c)
 
-def func01(a,o,c):
-    x=1
-    x=a+c
+def func01(a, o, c):
+    x = a + c
     print(f"{a} {o} {c} = {x}")
 
-def func02(a,o,c):
-    x=1
-    x=a-c
+def func02(a, o, c):
+    x = a - c
     print(f"{a} {o} {c} = {x}")
 
-def func03(a,o,c):
-    x=1
-    x=a/c
-    print(f"{a} {o} {c} = {x}")
+def func03(a, o, c):
+    x = a / c
+    print(f"{a} {o} {c} = {x:.2f}")
 
-def func04(a,o,c):
-    x=1
-    x=a*c
+def func04(a, o, c):
+    x = a * c
     print(f"{a} {o} {c} = {x}")
 
 if o == '+':
-    func01(a,o,c)
-
+    func01(a, o, c)
 elif o == '-':
-    func02(a,o,c)
-
+    func02(a, o, c)
 elif o == '/':
-    func03(a,o,c)
-
+    func03(a, o, c)
 elif o == '*':
-    func04(a,o,c)
-
+    func04(a, o, c)
 else:
     print("False")
