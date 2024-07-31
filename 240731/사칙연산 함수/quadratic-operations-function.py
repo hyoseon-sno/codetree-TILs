edc69ae91b3ee4480fc415e136b8cc1d5a725659
kimhyoseon <kimhyoseon928@gmail.com>
2024-07-31 18:47:39 +0900

    Title: 사칙연산 함수, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,38 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+def func01(a,o,c):
+    x=1
+    x=a+c
+    print(f"{a} {o} {c} = {x}")
+
+def func02(a,o,c):
+    x=1
+    x=a-c
+    print(f"{a} {o} {c} = {x}")
+
+def func03(a,o,c):
+    x=1
+    x=a/c
+    print(f"{a} {o} {c} = {x}")
+
+def func04(a,o,c):
+    x=1
+    x=a*c
+    print(f"{a} {o} {c} = {x}")
+
+if o == '+':
+    func01(a,o,c)
+
+elif o == '-':
+    func02(a,o,c)
+
+elif o == '/':
+    func03(a,o,c)
+
+elif o == '*':
+    func04(a,o,c)
+
+else:
+    print("False")
