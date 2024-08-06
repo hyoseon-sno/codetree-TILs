40aed10f630baeca8117f170117abe3b6f20c4f3
kimhyoseon <kimhyoseon928@gmail.com>
2024-08-06 19:52:20 +0900

    Title: 사는 지역, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240806/사는 지역/where-live.py
@@ -0,0 +1,19 @@
+class Address:
+    def __init__(self,name,address,region):
+        self.name=name
+        self.address=address
+        self.region=region
+
+n=int(input())
+arr=[tuple(input().split()) for _ in range(n)]
+
+people = [Address(name, address, region) for name, address, region in arr]
+
+target_idx = 0
+for i, person in enumerate(people):
+    if person.name > people[target_idx].name:
+        target_idx = i
+
+print(f"name {people[target_idx].name}")
+print(f"addr {people[target_idx].address}")
+print(f"city {people[target_idx].region}")
