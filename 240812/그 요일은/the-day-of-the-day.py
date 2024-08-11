75f316f808a80b1f2e30b4fd969111d40818cfa0
kimhyoseon <kimhyoseon928@gmail.com>
2024-08-12 00:42:35 +0900

    Title: 그 요일은, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/그 요일은/the-day-of-the-day.py
+++ 240812/그 요일은/the-day-of-the-day.py
@@ -1,36 +1,33 @@
-m1, d1, m2, d2 = map(int, input().split())
-
 def count_weekday_occurrences(m1, d1, m2, d2, target_day):
     month_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     day_name = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-    
-    total_days = 0
-    
-    total_days += month_days[m1] - d1 + 1
-    
-    for month in range(m1 + 1, m2):
-        total_days += month_days[month]
 
-    if m1 != m2:
-        total_days += d2
-    else:
-        total_days += d2 - d1 + 1
-
-    start_index = 0 
     target_index = day_name.index(target_day)
     
-    start_day_index = (start_index + total_days % 7) % 7
+    occurrences = 0
 
-    full_weeks = total_days // 7
-    remaining_days = total_days % 7
+    current_day_of_week = 0 
+    total_days = 0
+
+    for month in range(1, m1):
+        total_days += month_days[month]
     
-    occurrences = full_weeks
-    for i in range(remaining_days):
-        if (start_index + i) % 7 == target_index:
-            occurrences += 1
+    total_days += d1 - 1
     
+    current_day_of_week = (current_day_of_week + total_days) % 7
+    
+    for month in range(m1, m2 + 1):
+        start_day = d1 if month == m1 else 1
+        end_day = d2 if month == m2 else month_days[month]
+        
+        for day in range(start_day, end_day + 1):
+            if current_day_of_week == target_index:
+                occurrences += 1
+            current_day_of_week = (current_day_of_week + 1) % 7
+
     return occurrences
 
+m1, d1, m2, d2 = map(int, input().split())
 target_day = input()
 
 print(count_weekday_occurrences(m1, d1, m2, d2, target_day))
