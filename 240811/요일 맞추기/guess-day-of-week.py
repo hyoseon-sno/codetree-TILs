f78498a6f0f9f0bf362dd5f2fbbb254bf19f6acc
kimhyoseon <kimhyoseon928@gmail.com>
2024-08-11 12:54:53 +0900

    Title: 요일 맞추기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240811/요일 맞추기/guess-day-of-week.py
+++ 240811/요일 맞추기/guess-day-of-week.py
@@ -5,16 +5,26 @@ day_name = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 total_days = 0
 
-if m1 == m2:
-    total_days = d2 - d1
+if (m1 > m2) or (m1 == m2 and d1 > d2):
+    total_days += d2 - d1
+    
+    for i in range(m2, m1):
+        total_days -= month_day[i]
+
+    if m1 != m2:
+        total_days -= month_day[m1] - d1
+
 else:
-    total_days += month_day[m1] - d1
-
-    for i in range(m1 + 1, m2):
-        total_days += month_day[i]
-
-    total_days += d2
+    if m1 == m2:
+        total_days = d2 - d1
+    else:
+        total_days += month_day[m1] - d1
+        for i in range(m1 + 1, m2):
+            total_days += month_day[i]
+        total_days += d2
 
 index = total_days % 7
+
+index = (7 + index) % 7
 
 print(day_name[index])
