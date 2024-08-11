e7304f5c264988a7472d29b535c95e8f82dd00e4
kimhyoseon <kimhyoseon928@gmail.com>
2024-08-12 00:11:31 +0900

    Title: 요일 맞추기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/요일 맞추기/guess-day-of-week.py
+++ 240812/요일 맞추기/guess-day-of-week.py
@@ -3,22 +3,21 @@ m1, d1, m2, d2 = map(int, input().split())
 month_day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day_name = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-total_days = 0
+def day_difference(m1, d1, m2, d2):
+    if (m1 > m2) or (m1 == m2 and d1 > d2):
+        m1, d1, m2, d2 = m2, d2, m1, d1
+    
+    total_days = 0
+    if m1 == m2:
+        total_days = d2 - d1
+    else:
+        total_days += month_day[m1] - d1  
+        for i in range(m1 + 1, m2):
+            total_days += month_day[i]    
+        total_days += d2                
+    return total_days
 
-if m1 == m2:
-    total_days = d2 - d1
-elif m1 < m2:
-    total_days += month_day[m1] - d1  
-    for i in range(m1 + 1, m2):
-        total_days += month_day[i]    
-    total_days += d2                 
-
-else:
-    total_days -= d1 
-    for i in range(m2, m1):
-        total_days -= month_day[i]    
-    total_days -= (month_day[m2] - d2)
-
+total_days = day_difference(m1, d1, m2, d2)
 index = (total_days % 7 + 7) % 7
 
 print(day_name[index])
