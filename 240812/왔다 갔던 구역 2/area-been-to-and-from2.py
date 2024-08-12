977cb92b26d46fe3e909aba2dc5ac2b9a1a4ed4e
kimhyoseon <kimhyoseon928@gmail.com>
2024-08-12 23:53:24 +0900

    Title: 왔다 갔던 구역 2, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240812/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,45 +1,22 @@
-# n=int(input())
-# arr = [0] * (n * 10 * 2)
-# x = n * 10
-
-# for i in range(n):
-#     a,b=tuple(input().split())
-#     a=int(a)
-#     if b=="R":
-#         for j in range(x,x+a):
-#             arr[j]=arr[j]+1
-#         x=x+a
-#     elif b=="L":
-#         for j in range(x,x-a,-1):
-#             arr[j]+=1
-#         x=x-a
-
-# count=0
-# for num in arr:
-#     if num>1:
-#         count+=1
-
-# print(count)
-
-n = int(input())
-# 충분히 큰 크기의 배열 생성, 중간에 x=0을 배치하기 위해 n*10*2
+n=int(input())
 arr = [0] * (n * 10 * 2)
-# 시작 위치 설정 (중간에 위치)
 x = n * 10
 
-for _ in range(n):
-    a, b = input().split()
-    a = int(a)
-    if b == "R":
-        for j in range(x, x + a):
-            arr[j] += 1
-        x += a  # 위치 이동
-    elif b == "L":
-        for j in range(x, x - a, -1):
-            arr[j] += 1
-        x -= a  # 위치 이동
+for i in range(n):
+    a,b=tuple(input().split())
+    a=int(a)
+    if b=="R":
+        for j in range(x+1,x+a+1):
+            arr[j]=arr[j]+1
+        x=x+a
+    elif b=="L":
+        for j in range(x-1,x-a-1,-1):
+            arr[j]+=1
+        x=x-a
 
-# 2번 이상 지나간 영역의 크기 계산
-count = sum(1 for num in arr if num > 1)
+count=0
+for num in arr:
+    if num>1:
+        count+=1
 
 print(count)
