55e14d6b7d9cb7ebd2736bcf26c0d21d61432947
kimhyoseon <kimhyoseon928@gmail.com>
2024-08-13 00:27:27 +0900

    Title: 흰검 칠하기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240813/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,28 @@
+n=int(input())
+arr = [0] * (n * 10 * 2)
+arr_c = [0] * (n * 10 * 2)
+x = n * 10
+
+for i in range(n):
+    a, b = tuple(input().split())
+    a = int(a)
+    if b == "R":
+        for j in range(x, x + a):
+            arr[j] += 1
+            arr_c[j] = "B"
+        x += a
+    elif b == "L":
+        for j in range(x-1, x - a-1, -1):
+            arr[j] += 1
+            arr_c[j] = "W"
+        x -= a
+
+for k in range(len(arr)):
+    if arr[k]>3:
+        arr_c[k]="G"
+
+x=arr_c.count("W")
+y=arr_c.count("B")
+z=arr_c.count("G")
+
+print(x,y,x)
