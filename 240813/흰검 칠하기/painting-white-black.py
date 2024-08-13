7a192f4ba0a1310406646a087635e874d8075114
kimhyoseon <kimhyoseon928@gmail.com>
2024-08-13 09:39:38 +0900

    Title: 흰검 칠하기, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240813/흰검 칠하기/painting-white-black.py
+++ 240813/흰검 칠하기/painting-white-black.py
@@ -1,29 +1,28 @@
 n=int(input())
-arr = [0] * (n * 10 * 2)
-arr_c = [0] * (n * 10 * 2)
-x = n * 10-1
+arr = [0] * (20000 + 1) 
+arr_c = [" "] * (20000 + 1)
+x = 10000  
 
 for i in range(n):
-    a, b = tuple(input().split())
+    a, b = input().split()
     a = int(a)
     if b == "R":
         for j in range(x, x + a):
             arr[j] += 1
             arr_c[j] = "B"
-        x=x+a-1
+        x = x + a - 1  
     elif b == "L":
         for j in range(x, x - a, -1):
             arr[j] += 1
             arr_c[j] = "W"
-        x -= a
-        x=x+1
-
+        x = x - a
+        x = x + 1  
 for gray in range(len(arr)):
-    if arr[gray]>3:
-        arr_c[gray]="G"
+    if arr[gray] > 3:
+        arr_c[gray] = "G"
 
-x=arr_c.count("W")
-y=arr_c.count("B")
-z=arr_c.count("G")
+x = arr_c.count("W")
+y = arr_c.count("B")
+z = arr_c.count("G")
 
-print(x,y,z)
+print(x, y, z)
