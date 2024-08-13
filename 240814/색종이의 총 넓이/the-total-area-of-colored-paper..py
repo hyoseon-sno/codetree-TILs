bd610a863fb2964c634931c24641cd074febb634
kimhyoseon <kimhyoseon928@gmail.com>
2024-08-14 00:35:47 +0900

    Title: 색종이의 총 넓이, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 240814/색종이의 총 넓이/the-total-area-of-colored-paper..py
+++ 240814/색종이의 총 넓이/the-total-area-of-colored-paper..py
@@ -1,20 +1,14 @@
 n = int(input())
-arr = [[0 for _ in range(300)] for _ in range(300)]
-
-x, y = 100,100
+arr = [[0 for _ in range(200)] for _ in range(200)]
 
 for _ in range(n):
     a, b = map(int, input().split())
-    x += a
-    y += b
+    x = a + 100
+    y = b + 100
     for i in range(x, x + 8):
         for j in range(y, y + 8):
             arr[i][j] = 1  
 
-result = 0
-for i in range(300):
-    for j in range(300):
-        if arr[i][j] == 1:
-            result += 1
+result = sum(sum(row) for row in arr)
 
 print(result)
