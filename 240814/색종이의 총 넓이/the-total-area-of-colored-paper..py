19a7f8842ee9a199897eaa5126707498b6c28360
kimhyoseon <kimhyoseon928@gmail.com>
2024-08-14 00:40:15 +0900

    Title: 색종이의 총 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240814/색종이의 총 넓이/the-total-area-of-colored-paper..py
+++ 240814/색종이의 총 넓이/the-total-area-of-colored-paper..py
@@ -1,13 +1,19 @@
 n = int(input())
-arr = [[0 for _ in range(200)] for _ in range(200)]
+arr = [[0 for _ in range(300)] for _ in range(300)]
 
 for _ in range(n):
     a, b = map(int, input().split())
-    x = a + 100
-    y = b + 100
+    x += a
+    y += b
     for i in range(x, x + 8):
         for j in range(y, y + 8):
             arr[i][j] = 1  
+
+result = 0
+for i in range(300):
+    for j in range(300):
+        if arr[i][j] == 1:
+            result += 1
 
 result = sum(sum(row) for row in arr)
 
