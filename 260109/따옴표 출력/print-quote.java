98571f6bfe5c82dac2551f5dabc84442b59170fe
kimhyoseon <kimhyoseon928@gmail.com>
2026-01-09 14:38:01 +0900

    Title: 따옴표 출력, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

--- 260109/따옴표 출력/print-quote.java
+++ 260109/따옴표 출력/print-quote.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        String a = "He says \"It's a really simple sentence\".";
-
-        System.out.print(a);
+        System.out.print("He says \"It's a really simple sentence\".");
     }
 }
