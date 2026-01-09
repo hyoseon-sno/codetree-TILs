8c2aad090b517a65ebdcc3d539a4f0e7e0d65f56
kimhyoseon <kimhyoseon928@gmail.com>
2026-01-09 14:42:16 +0900

    Title: 숫자 2개 출력, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260109/숫자 2개 출력/print-two-numbers.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int a=3;
+        int b=5;
+
+        System.out.printf("%d %d",a,b);
+
+    }
+}
