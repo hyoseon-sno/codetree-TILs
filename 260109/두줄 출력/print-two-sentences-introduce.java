dda2f84eaaa668edb9221b08fbe62639d4e07433
kimhyoseon <kimhyoseon928@gmail.com>
2026-01-09 14:54:17 +0900

    Title: 두줄 출력, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260109/두줄 출력/print-two-sentences-introduce.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String a="Hello students!";
+        String b="Welcome to LeebrosCode!";
+        System.out.printf("%s\n%s",a,b);
+    }
+}
