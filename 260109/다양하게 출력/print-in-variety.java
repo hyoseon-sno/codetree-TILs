ee19d54cfb138f1372fb8f0f1cf078ab70c419f4
kimhyoseon <kimhyoseon928@gmail.com>
2026-01-09 14:57:49 +0900

    Title: 다양하게 출력, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260109/다양하게 출력/print-in-variety.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        String a = "Total days in Year";
+        int b = 365;
+        String c = "Circumference rate";
+        double d = 3.1415926535;
+
+        System.out.printf("%s\n%d\n%s\n%.10f", a, b, c, d);
+
+    }
+}
